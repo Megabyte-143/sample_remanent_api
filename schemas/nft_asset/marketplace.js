e741de46efbbed65caba76c8ce47a5faeb32a696
Yash Garg <yashygkf@gmail.com>
2022-06-10 16:27:19 +0530

    add: marketplace schema

---
+++ schemas/nft_asset/marketplace.js
@@ -0,0 +1,12 @@
+export const marketplaceSchema = {
+    type: 'object',
+    properties: {
+        name:
+        {
+            type: 'string',
+        },
+        url: {
+            type: 'string',
+        },
+    },
+};
