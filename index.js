96201e6fd4f576d6d2600773791f002cf7b74583
Yash Garg <yashygkf@gmail.com>
2022-06-27 02:14:46 +0530

    feat: nft_transactions and nft_assets through NFT Port

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// * imports
 import express, { urlencoded, json } from 'express';
 import fetch from 'node-fetch';
 
 // * API Models
-// import nft_transactionSchema from './models/nft_transaction/nft_transaction.js';
+import nft_transactionSchema from './models/nft_transaction/nft_transaction.js';
 import nft_assetSchema from './models/nft_asset/nft_asset.js';
 import nft_collectionSchema from './models/nft_collection/nft_collection.js';
 
@@ -15,13 +14,14 @@ app.use(json()); // to accept the server data
 // * Deployment Ports
 const port = 8080;
 
-
-// * Base URLs
-const baseUrl = {
-    "1": "https://eth-mainnet.alchemyapi.io/v2/uEQQbEYufrEyrWbuNnfnTol_LgMKUfQn",
-    "137": "https://polygon-mainnet.g.alchemy.com/v2/Ldh3zmCuAdHAsJATa51xXzF14sGYbwuX",
+// * Chains
+const chain = {
+    "1": 'ethereum',
+    "137": 'polygon',
 };
 
+// * Base URL
+let baseUrl = 'https://api.nftport.xyz/v0/';
 
 
 //============================================================================================
@@ -35,102 +35,79 @@ const baseUrl = {
 */
 
 app.get('/v1/web3/nfts/:chain_id/:ownerAdd', async (req, res) => {
-    let fetchUrl = `${baseUrl[req.params.chain_id]}/getNFTs/?owner=${req.params.ownerAdd}`;
 
-    let nft_asset = new nft_assetSchema();
-
+    let nft_assets = new nft_assetSchema();
     let responseData = [];
 
-    const response = await fetch(fetchUrl);
-
-    await response.json().then(data => {
-        data.ownedNfts.forEach(nft => {
-            nft_asset.data = {
-                id: nft.id.tokenId,
-                token_id: nft.id.tokenId,
-                chain: req.params.chain_id,
-                name: nft.metadata.name,
-                description: nft.metadata.description,
-                file_url: nft.metadata.image,
-                associated_url: nft.metadata.external_url,
-                // traits: nft.metadata.attributes.map(trait => {
-                //     return {
-                //         value: trait.value,
-                //         type: trait.trait_type,
-                //     };
-                // })
-                traits:[ JSON.stringify(nft.metadata.attributes)]
-                ,
-            };
-            responseData.push(nft_asset.data);
-        })
-        res.send(responseData);
-    }).catch(err => {
-        console.log(err);
-    });
-});
-
-// ===========================================================================================
-
-/*   
-    *Route               /v1/web3/nfts/
-    * Description        Get all the NFTs from the particular chain of a particular contract address
-    *Access              PUBLIC
-    *Parameter           chain_id, contract_address
-    *Methods             GET
-*/
-
-app.get('/v1/web3/nft_collections/:chain_id/:contract_address', async (req, res) => {
-
-    let fetchUrl = `${baseUrl[req.params.chain_id]}/getNFTsForCollection?contractAddress=${req.params.contract_address}&withMetadata=true`;
-    let ownerDataUrl = `${baseUrl[req.params.chain_id]}/getOwnersForCollection?contractAddress=${req.params.contract_address}`;
-
-    // console.log(fetchUrl);
-    // let nft_collection = new nft_collectionSchema();
-    let responseData = [];
-    let nft_collection = new nft_collectionSchema();
-    const response = await fetch(fetchUrl);
-    const ownerDataResponse = await fetch(ownerDataUrl);
-    await ownerDataResponse.json().then(data => {
-        responseData.push(data);
-
-    }).catch(err => {
-        console.log(err);
-    });
-    await response.json().then(data => {
-
-        // console.log(data);
-
+    let options = {
+        method: 'GET',
+        qs: {
+            chain: 'ethereum', page_size: '5', include: 'metadata'
+        },
+        headers: {
+            Authorization: '364f1aa2-aafa-4fca-8f92-f2a775aead89'
+        }
+    };
+    let fetchUrl = `${baseUrl}accounts/${req.params.ownerAdd}`;
+    (await fetch(fetchUrl, options)).json().then(data => {
+        console.log(data);
         data.nfts.forEach(nft => {
-
-            nft_collection.data = {
-                id: nft.id.tokenId,
-                chain: req.params.chain_id == 137 ? 'polygon' : 'eth',
-                contract: {
-                    address: nft.contract.address,
-                },
-                name: nft.title,
+            nft_assets.data.properties = {
+                token_id: nft.token_id,
+                chain: chain[req.params.chain_id],
+                name: nft.name,
                 description: nft.description,
-                // logo_url: nft.metadata.image,
-                assets: {
-                    id: nft.id.tokenId,
-                    name: nft.title,
-                    token_id: nft.id.tokenId,
-                },
-                traits:[ JSON.stringify(nft.metadata.attributes)],
+                file_url: nft.file_url,
+                associated_url: nft.animation_url,
+                creators: [nft.creator_address],
+                traits: [
+                    JSON.stringify(nft.metadata.attributes)
+                ],
+                collection: {
+                    name: nft.contract.name,
+                    contract_address: nft.contract.address,
+                }
             }
-            // console.log(nft_collection.data.traits);
-            // console.log(nft)
-
-            responseData.push(nft_collection.data);
+            responseData.push(nft_assets.data);
         });
-        res.send(responseData);
-        // console.log(responeData);
-    }).catch(err => {
-        console.log(err);
-    });
+        res.send(nft_assets.data);
+    }).catch(err => console.error('error:' + err));
 });
 
+app.get('/v1/web3/nfts/transactions/:chain_id', async (req, res) => {
+    let nft_transactions = new nft_transactionSchema();
+    let responseData = [];
+
+
+    let options = {
+        method: 'GET',
+        qs: { chain: 'ethereum', type: 'all' },
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: '364f1aa2-aafa-4fca-8f92-f2a775aead89'
+        }
+    };
+    let fetchUrl = `${baseUrl}transactions`;
+
+    (await fetch(fetchUrl, options)).json().then(data => {
+        console.log(data);
+        data.transactions.map(transaction => {
+            nft_transactions.data.properties = {
+
+                token_id: transaction.token_id,
+                contract: {
+                    address: transaction.contract_address,
+                },
+                sender: transaction.transfer_from,
+                receivers: [JSON.stringify(transaction.transfer_to)],
+
+            }
+        })
+    }).catch(err => console.error('error:' + err));
+
+
+
+});
 
 
 // * Start the server
--- models/nft_transaction/nft_transaction.js
+++ models/nft_transaction/nft_transaction.js
@@ -1,54 +1,56 @@
- import { nft_collection_royaltySchema } from '../nft_collection_royalty.js';  
- import { nft_collection_contractSchema} from '../nft_collection_contract.js';
+import { nft_collection_royaltySchema } from '../nft_collection_royalty.js';
+import { nft_collection_contractSchema } from '../nft_collection_contract.js';
 
-const nft_transactionSchema = {
-    type: 'object',
-    properties: {
-        id: {
-            type: 'string',
-            format: 'uuid' ,
-        },
-        token_id: {
-            type: 'string',
-        },
-        nft_asset_id: {
-            type: 'string',
-        },
-        contract: {
-            type: nft_collection_contractSchema
-        },
-        sender: {
-            type: 'string',
-        },
-        creator:{
-            type: [String],
-        },
-        receivers: {
-            type: [String],
-        },
-        marketplace:{
-            type: 'string',
-        },
-        price: {
-            type: 'string',
-        },
-        type:{
-            enum: ['MINT', 'LIST', 'TRANSFER', 'PURCHASE', 'DELIST', 'SELL'],
-        },
-        signature: {
-            type: 'string',
-        },
-        royalty: {
-            type: nft_collection_royaltySchema,
-        },
-        currency: {
-            type : 'string',
-        },
-        timestamp: {
-            type: 'string',
-        },
-    },
-    required: ['id', 'token_id', 'nft_asset_id', 'contract', 'sender', 'creator', 'receivers', 'price', 'type', 'signature', 'royalty', 'currency', 'timestamp'],
-};
+class nft_transactionSchema {
+    data = {
 
+        type: 'object',
+        properties: {
+            id: {
+                type: 'string',
+                format: 'uuid',
+            },
+            token_id: {
+                type: 'string',
+            },
+            nft_asset_id: {
+                type: 'string',
+            },
+            contract: {
+                type: nft_collection_contractSchema
+            },
+            sender: {
+                type: 'string',
+            },
+            creator: {
+                type: [String],
+            },
+            receivers: {
+                type: [String],
+            },
+            marketplace: {
+                type: 'string',
+            },
+            price: {
+                type: 'string',
+            },
+            type: {
+                enum: ['MINT', 'LIST', 'TRANSFER', 'PURCHASE', 'DELIST', 'SELL'],
+            },
+            signature: {
+                type: 'string',
+            },
+            royalty: {
+                type: nft_collection_royaltySchema,
+            },
+            currency: {
+                type: 'string',
+            },
+            timestamp: {
+                type: 'string',
+            },
+        },
+        required: ['id', 'token_id', 'nft_asset_id', 'contract', 'sender', 'creator', 'receivers', 'price', 'type', 'signature', 'royalty', 'currency', 'timestamp'],
+    };
+}
 export default nft_transactionSchema;
